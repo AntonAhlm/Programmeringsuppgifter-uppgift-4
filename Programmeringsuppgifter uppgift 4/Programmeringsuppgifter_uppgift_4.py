5e982ea4ea9240597076005b667a499b50ca1f53
Admin <Admin@TEB123012>
2022-11-16 10:21:48 +0100

    Add project files.

---
+++ Programmeringsuppgifter uppgift 4/Programmeringsuppgifter_uppgift_4.py
@@ -0,0 +1,10 @@
+ruta=" "*50
+print(ruta,"+","-"*16,"+")
+print(ruta,"|  Te21C           |")
+print(ruta,"|  Anton Ahlm      |")
+print(ruta,"+","-"*16,"+\n\n")
+
+tal1=int(input("Tal 1: "))
+tal2=int(input("Tal 2: ")) 
+m=(tal1+tal2)/2
+print("Medelvardet ar ",m,)
